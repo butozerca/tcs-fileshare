c0fb50fd51bea829d0e46b2bae6c0887ba90f2ff
patryku <patryk.u93@gmail.com>
2014-01-16 22:35:57 +0100

    wstepny szkielet klienta

--- fileshare/src/Client.java
+++ fileshare/src/Client.java
@@ -1,8 +1,38 @@
-
+import java.io.IOException;
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Client {
 
 	/**
 	 * Klasa odpowiedzialna za wysylanie requestow i odbieranie danych
 	 */
+
+	private String serverAddress;
+	private int destPort;
+	private String query;
+
+	public Client(String serverAddress, int destPort, String query) {
+		this.serverAddress = serverAddress;
+		this.destPort = destPort;
+		this.query = query;
+	}
+
+	public String getReply() {
+		String reply = null;
+		try {
+			Socket client = new Socket(serverAddress, destPort);
+			DataOutputStream out = new DataOutputStream(client.getOutputStream());
+			out.writeUTF(query);
+			DataInputStream in = new DataInputStream(client.getInputStream());
+			reply = in.readUTF();
+			client.close();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return reply;
+	}
 }
