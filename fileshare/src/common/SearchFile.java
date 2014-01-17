4dd1e039989b1ae1b95ed7b54af2d70efdaa2892
Michał Dyrek <butozerca@gmail.com>
2014-01-17 22:22:08 +0100

    small fix to searchfile

--- fileshare/src/common/SearchFile.java
+++ fileshare/src/common/SearchFile.java
@@ -6,19 +6,20 @@ import java.util.Scanner;
 
 public class SearchFile {
 
-	public static ArrayList<String> searchFile(String name, File file)
+	public static ArrayList<String> searchFile(String filename, String directory)
     {
+		File file = new File(directory);
         File[] list = file.listFiles();
         if(list.length == 0) return null;
         ArrayList<String> res = new ArrayList<String>(), ret;
         
         for (File fil : list) {
         	if( fil.isDirectory() ){
-        		ret = searchFile(name, fil);
+        		ret = searchFile(filename, fil.getPath());
         		if(ret != null)
         			res.addAll(ret);
         	}
-        	else if(fil.getName().contains(name))
+        	else if(fil.getName().contains(filename))
         		res.add(fil.getAbsolutePath());
         }
         return res;
@@ -29,7 +30,7 @@ public class SearchFile {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the directory where to search ");
         String directory = scan.next();
-        ArrayList<String> res = SearchFile.searchFile("User.java", new File(directory));
+        ArrayList<String> res = SearchFile.searchFile("User.java", directory);
         for(String s : res)
         	System.out.println(s);
         scan.close();
